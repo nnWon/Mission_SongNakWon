6c50dafacfc2b43baca8baf95620d5e9a23ef484
nnWon <nwwn4627@gmail.com>
2023-11-06 11:38:56 +0900

    feat: 4단계 완료
    등록할 때마다 명언 번호가 증가

--- src/main/java/com/ll/service/OrderService.java
+++ src/main/java/com/ll/service/OrderService.java
@@ -1,16 +1,24 @@
 package com.ll.service;
 
+import com.ll.domain.Quote;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class OrderService {
 
+    private final Map<Integer, Quote> storage = new HashMap<>();
+    private int sequence = 0;
+
     public void execute(Scanner scanner, String order) {
-        if (order.equals("등록")){
+        if (order.equals("등록")) {
             System.out.print("명언 : ");
             String quote = scanner.nextLine();
             System.out.print("작가 : ");
             String speaker = scanner.nextLine();
-            System.out.println("명언이 등록되었습니다.");
+            storage.put(++sequence, new Quote(quote, speaker));
+            System.out.println(sequence + "번 명언이 등록되었습니다.");
         }
     }
 }
