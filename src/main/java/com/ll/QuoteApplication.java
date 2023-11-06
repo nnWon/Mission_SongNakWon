828b3a2e74e206d6a0dde6435e30cb907cf5f2fd
nnWon <nwwn4627@gmail.com>
2023-11-06 10:08:44 +0900

    feat: 2단계 완료
    "명령) 등록"등 을 추가 출력한다.

--- src/main/java/com/ll/QuoteApplication.java
+++ src/main/java/com/ll/QuoteApplication.java
@@ -3,6 +3,9 @@ package com.ll;
 public class QuoteApplication {
     public void run(){
         System.out.println("== 명언 앱 ==");
+        System.out.println("명령) 등록 ");
+        System.out.println("명언 : 현재를 사랑하라.");
+        System.out.println("작가 : 작자미상");
         System.out.println("명령) 종료");
     }
 }
