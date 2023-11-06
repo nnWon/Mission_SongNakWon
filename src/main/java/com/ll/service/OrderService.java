089f87faa2160d05551097fe4160e1814dd502b8
nnWon <nwwn4627@gmail.com>
2023-11-06 11:48:01 +0900

    refactor: OrderService 명언등록 메서드 분리

--- src/main/java/com/ll/service/OrderService.java
+++ src/main/java/com/ll/service/OrderService.java
@@ -13,12 +13,17 @@ public class OrderService {
 
     public void execute(Scanner scanner, String order) {
         if (order.equals("등록")) {
-            System.out.print("명언 : ");
-            String quote = scanner.nextLine();
-            System.out.print("작가 : ");
-            String speaker = scanner.nextLine();
-            storage.put(++sequence, new Quote(quote, speaker));
-            System.out.println(sequence + "번 명언이 등록되었습니다.");
+            createQuote(scanner);
         }
+    }
+
+    public int createQuote(Scanner scanner) {
+        System.out.print("명언 : ");
+        String quote = scanner.nextLine();
+        System.out.print("작가 : ");
+        String speaker = scanner.nextLine();
+        storage.put(++sequence, new Quote(quote, speaker));
+        System.out.println(sequence + "번 명언이 등록되었습니다.");
+        return sequence;
     }
 }
