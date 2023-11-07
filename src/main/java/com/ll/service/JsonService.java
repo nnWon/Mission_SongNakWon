75d367b6f48c271901f8367fc8344cc8e93d52f9
nnWon <nwwn4627@gmail.com>
2023-11-07 17:12:09 +0900

    feat: 10단계 완료
    - JsonService의 build() 메서드 작성
    - "빌드" 명령어 입력시 Json형식 파일로 저장하는 기능 구현

--- src/main/java/com/ll/service/JsonService.java
+++ src/main/java/com/ll/service/JsonService.java
@@ -1,11 +1,19 @@
 package com.ll.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.domain.Quote;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonService {
 
-    public void build(Map<Integer, Quote> storage) {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private final static String FILE_PATH = "./file/";
+    private final static String JSON_FILE_NAME = "quotes_json.txt";
+
+    public void build(Map<Integer, Quote> storage) throws IOException {
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(FILE_PATH+JSON_FILE_NAME), storage);
     }
 }
