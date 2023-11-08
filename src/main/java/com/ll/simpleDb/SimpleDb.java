b62e930dec6a3faa4728fadfb2901656230bd5cb
nnWon <nwwn4627@gmail.com>
2023-11-08 16:34:23 +0900

    test: SimpleDb createArticleTable() 테스트 코드 작성

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -3,8 +3,7 @@ package com.ll.simpleDb;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SimpleDb {
 
@@ -16,20 +15,58 @@ public class SimpleDb {
     }
 
     private String urlFormatting(String url) {
-        return "jdbc:mysql://"+url+":3306/";
+        return "jdbc:mysql://" + url + ":3306/";
     }
 
-    private DataSource initDataSource(String url, String username, String password, String dbName){
+    private DataSource initDataSource(String url, String username, String password, String dbName) {
         HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(url+dbName);
+        dataSource.setJdbcUrl(url + dbName);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
-        dataSource.setMaximumPoolSize(10);
-        dataSource.setPoolName("MyPool");
         return dataSource;
     }
 
     public Connection getConnection() throws SQLException {
         return this.dataSource.getConnection();
+    }
+
+    public void run(String sql) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -1,5 +1,6 @@
 package com.ll.simpleDb;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -13,11 +14,34 @@ public class SimpleDbTest {
 
     private SimpleDb simpleDb;
 
+    @BeforeAll
+    public void beforeAll() throws SQLException {
+        simpleDb = new SimpleDb("localhost", "root", "test11", "simpleDb__test");
+        //simpleDb.setDevMode(true);
+        createArticleTable();
+    }
+
+    private void createArticleTable() throws SQLException {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""                                                
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
+    }
+
     @Test
     void DbConnectionTest() throws SQLException {
         simpleDb = new SimpleDb("localhost", "root", "test11", "simpleDb__test");
         Connection connection = simpleDb.getConnection();
-        System.out.println("connection = "+connection);
-        System.out.println("connection.getClass() = "+connection.getClass());
+        System.out.println("connection = " + connection);
+        System.out.println("connection.getClass() = " + connection.getClass());
     }
 }
